931302defe1830403f41e379f9652d7477f1e048
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-03-27 16:40:27 +0800

    Initial commit

---
+++ PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Liko Setiawan on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
